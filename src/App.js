932b8138984ec87fd345f01b41968cd313f3e56b
kfwang177880 <kfwang177880@yahoo.com>
2020-08-03 16:05:09 -0400

    completed typing but getting 1 failure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,111 @@
-import React {Component} from 'react';
+import React, {Component} from 'react'  
 
-class App extends Component {
-  
-  render() {
-    return (
-      <div> </div>
-    )
-  };
-}
+  class App extends Component {
+    constructor () {
+      super();
+      this.state = {
+        items: [],
+        isLoaded: false,
+      }
+    }
+
+    componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        this.setState({
+          isLoaded: true,
+          items: data,
+        });
+      });
+    }
+
+    render() {
+      var { isLoaded, items } = this.state;
+      if (!isLoaded) {
+        return <div>Loading....</div>;
+      } else {
+        return (
+          <div className="App">
+            <div className="Names">
+              <ul>
+                {items.map (el => {
+                  return (
+                    <li key = {el.id}>
+                      Name: {el.name} | UserName: {el.username} | {' '}
+                      <a href={`https://${el.website}`}>Website</a>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          </div>
+        );
+      }
+    };
+  }
 
 export default App;
+
+
+
+
+
+/*
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  render() {
+    return (
+      <div>=</div>
+    )
+  };
+/*  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        this.setState({
+          isLoaded: true,
+          items: data,
+        });
+      });
+  }
+
+  render() {
+    var {isLoaded, items} = this.state;
+    if (!isLoaded) {
+      return <div>Loading....</div>;
+    } else {
+      return (
+        <div className="App">
+          <div className="Names">
+            <ul>
+              {items.map(el => {
+                return (
+                  <li key={el.id}>
+                    Name: {el.name} | UseName: {el.username} | {' '}
+                    <a href={`https://${el.website}`}>Webiste</a>
+                  </li>
+                );
+              })}
+          </ul>
+        </div> 
+      </div>
+    );
+  }
+*/
+
